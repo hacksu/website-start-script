bd545c5b395ce439ed7316f45b880014b1a94085
Paul Dilyard <paul.dilyard@gmail.com>
2014-12-30 19:10:40 -0500

    Uses hacksu user

--- start.sh
+++ start.sh
@@ -7,10 +7,6 @@ yum install -y nodejs npm
 npm install -g n
 n stable
 
-# Add a web user
-echo "Creating a web user..."
-useradd -mrU web
-
 # Install nginx
 echo "Installing nginx..."
 yum install nginx
@@ -26,10 +22,9 @@ sudo service nginx start
 # Clone the app
 echo "Cloning the app from github..."
 mkdir -p /var/www
-chown web /var/www
-chgrp web /var/www
+chown hacksu /var/www
+chgrp hacksu /var/www
 cd /var/www
-su web
 [ -d Website ] && rm -rf Website
 git clone https://github.com/hacksu/Website
 cd Website
