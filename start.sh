a610d4f42ca71f0dfc331eb9de9f819d1b6c9d41
Paul Dilyard <paul.dilyard@gmail.com>
2014-12-30 19:05:38 -0500

    Only creates directories if they do not exist

--- start.sh
+++ start.sh
@@ -3,29 +3,13 @@
 # Install node
 echo "Installing/updating node..."
 yum update -y
-yum install -y git nodejs npm
+yum install -y nodejs npm
 npm install -g n
 n stable
 
 # Add a web user
 echo "Creating a web user..."
 useradd -mrU web
-
-# Clone the app
-echo "Cloning the app from github..."
-mkdir /var/www
-chown web /var/www
-chgrp web /var/www
-cd /var/www
-su web
-rm -rf Website
-git clone https://github.com/hacksu/Website
-
-# Install forever
-echo "Installing forever package..."
-npm install -g forever
-cd Website
-forever start app.js
 
 # Install nginx
 echo "Installing nginx..."
@@ -39,4 +23,21 @@ cp hacksu.conf /etc/nginx/conf.d/
 echo "Starting nginx..."
 sudo service nginx start
 
+# Clone the app
+echo "Cloning the app from github..."
+mkdir -p /var/www
+chown web /var/www
+chgrp web /var/www
+cd /var/www
+su web
+[ -d Website ] && rm -rf Website
+git clone https://github.com/hacksu/Website
+cd Website
+
+# Install forever
+echo "Installing forever package..."
+npm install -g forever
+forever start app.js
+
 echo "All done, enjoy the site!"
+cd ~
